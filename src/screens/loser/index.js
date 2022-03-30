3f201285c14a3af1d60e0dd009c0c686f88b5f39
hadilfriaa <hadil.friaa@ynov.com>
2022-03-30 18:32:06 +0200

    up ( j'ai oublié de push)

--- src/screens/loser/index.js
+++ src/screens/loser/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import { Title } from 'react-native-paper';
+import { Text, View, Image } from 'react-native'
 import styled from 'styled-components'
-
+import LogoVirgule from '../../assets/nike.png'
+import LoserImage from '../../assets/loser.png'
 
 
 const Loser = ({ navigation }) => {
 
     return (
         <ViewStyle>
-            <Title> T'as perdu loser à la prochaine inchaallah</Title>
+
+            <LosersImage source={LoserImage}></LosersImage>
+            <TextTitle> Oups ! Vous n'avez pas été selectionné </TextTitle>
+            <Text> Tentez votre chance une prochaine fois </Text>
+            <Text> À très bientôt ! </Text>
+            <TextLeft> Incha'allah ♥ </TextLeft>
+            <ImageFooter source={LogoVirgule}></ImageFooter>
+
         </ViewStyle>
 
 
@@ -17,6 +24,30 @@ const Loser = ({ navigation }) => {
 }
 const ViewStyle = styled.View`
     alignItems: center;
+    marginTop: 80px
+    marginLeft: 20px
+    marginRight: 20px
+
+`
+const TextTitle = styled.Text`
+    fontSize: 20px
+    marginBottom: 20px
+
+`
+const TextLeft = styled.Text`
+    marginTop: 20px
+    marginLeft: auto
+
+`
+const ImageFooter = styled.Image`
+    marginTop: 100px
+    width: 200px
+    height: 100px
+`
+const LosersImage = styled.Image`
+    marginTop: 10px
+    width: 70px
+    height: 70px
 `
 
 export default Loser
--- src/screens/winner/index.js
+++ src/screens/winner/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-import { Title } from 'react-native-paper';
+import { Text, View, Image } from 'react-native'
 import styled from 'styled-components'
-
+import LogoVirgule from '../../assets/nike.png'
+import Trophy from '../../assets/trophy.png'
 
 
 const Winner = ({ navigation }) => {
 
     return (
         <ViewStyle>
-            <Title> Gagné psahtek </Title>
+
+            <ImageTrophy source={Trophy}></ImageTrophy>
+
+            <TextTitle>  Gagné  </TextTitle>
+            <Text> Félicitation vous venez d'être selectionné </Text>
+            <Text> Prochaine étape : paiement + adresse de livraison ! </Text>
+            <TextLeft> En esperant que vous avez de quoi payer..♥ </TextLeft>
+            <ImageFooter source={LogoVirgule}></ImageFooter>
+
         </ViewStyle>
 
 
@@ -17,6 +25,30 @@ const Winner = ({ navigation }) => {
 }
 const ViewStyle = styled.View`
     alignItems: center;
+    marginTop: 80px
+    marginLeft: 20px
+    marginRight: 20px
+
+`
+const TextTitle = styled.Text`
+    fontSize: 20px
+    marginBottom: 20px
+
+`
+const TextLeft = styled.Text`
+    marginTop: 20px
+    marginLeft: auto
+
+`
+const ImageFooter = styled.Image`
+    marginTop: 100px
+    width: 200px
+    height: 100px
+`
+const ImageTrophy = styled.Image`
+    marginTop: 10px
+    width: 70px
+    height: 70px
 `
 
 export default Winner
